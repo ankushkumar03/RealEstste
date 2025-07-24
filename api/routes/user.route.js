55c549a2a8164fa42680c1ef2ce44ab137de7f2a
Ankush Kumar <131608050+ankushkumar03@users.noreply.github.com>
2025-07-24 14:21:18 +0530

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import { test, updateUser,deleteUser, getUserListings, getUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
-
-
 const router = express.Router();
+
 router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser )
-router.delete('/delete/:id', verifyToken, deleteUser )
+router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
+
+// ✅ Specific first
 router.get('/listings/:id', verifyToken, getUserListings);
+
+// ✅ Generic after
 router.get('/:id', verifyToken, getUser);
+
 export default router;
